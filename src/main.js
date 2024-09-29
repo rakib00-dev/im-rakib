1625f43277b90bee4815ddbce0484b72e1a56b33
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-09-29 19:28:34 +0600

    updated the event handler to event listener and added cursor follower

--- src/main.js
+++ src/main.js
@@ -1,18 +1,27 @@
+// cursor follower
+const cursor = document.querySelector('.cursor');
+window.addEventListener('mousemove', (e) => {
+  cursor.setAttribute(
+    'style',
+    `top: ${e.pageY - 40}px; left:${e.pageX - 30}px`
+  );
+  console.log(`top: ${e.pageY}; left:${e.pageX}`);
+});
+
 // window onLoad Entry
 const navBarScroll = document.getElementById('navBarFix');
 const headerSection = document.getElementById('header-section');
 
-window.onload = () => {
+window.addEventListener('load', () => {
   navBarScroll.style.top = '5px';
   navBarScroll.style.opacity = '1';
   headerSection.style.top = '0';
   headerSection.style.opacity = '1';
-};
-console.log(navBarScroll);
+});
 
 // header NavBar onScroll
 
-window.onscroll = () => {
+window.addEventListener('scroll', () => {
   const scrollValue = window.scrollY;
   if (scrollValue >= 82) {
     navBarScroll.style.top = '20px';
@@ -21,4 +30,4 @@ window.onscroll = () => {
     navBarScroll.style.top = '5px';
     navBarScroll.style.position = null;
   }
-};
+});
