4b3ef1f80b0daaf4a7b378d56a34a94ed7d842fa
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-01-21 22:08:46 +0600

    added new components skills.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from 'react';
 import Header from './Components/Header';
+import Skills from './Components/Skills';
 
 function App() {
   // cursor follower
@@ -34,51 +35,7 @@ function App() {
     <>
       <div className="cursor" ref={cursor}></div>
       <Header />
-      <section id="skills-section">
-        <div className="wrapper">
-          <h1 id="section-tittle">Skills</h1>
-          <div id="skills-wrap">
-            <div className="skill-box">
-              <img src="./images/skills/html-5.png" alt="" loading="lazy" />
-            </div>
-            <div className="skill-box">
-              <img src="./images/skills/css3.png" alt="" loading="lazy" />
-            </div>
-            {/* <div className="skill-box">
-            <img src="./images/skills/bootstrap.png" alt="" loading="lazy" />
-          </div>  */}
-            <div className="skill-box">
-              <img
-                src="./images/skills/react-1-logo-png-transparent.png"
-                alt="react js"
-                loading="lazy"
-              />
-            </div>
-            <div className="skill-box">
-              <img src="./images/skills/tailwind.png" alt="" loading="lazy" />
-            </div>
-            <div className="skill-box">
-              <img src="./images/skills/js.png" alt="" loading="lazy" />
-            </div>
-            <div className="skill-box">
-              <img src="./images/skills/social.png" alt="" loading="lazy" />
-            </div>
-            <div className="skill-box">
-              <img src="./images/skills/github.png" alt="" loading="lazy" />
-            </div>
-            <div className="skill-box">
-              <img src="./images/skills/bug.png" alt="" loading="lazy" />
-            </div>
-            <div className="skill-box">
-              <img src="./images/skills/photoshop.png" alt="" loading="lazy" />
-            </div>
-            <div className="skill-box">
-              <img src="./images/skills/daisyui.png" alt="" loading="lazy" />
-            </div>
-          </div>
-        </div>
-      </section>
-
+      <Skills />
       <section id="testimonial-section">
         <div className="wrapper">
           <h1 id="section-tittle">Testimonial</h1>
