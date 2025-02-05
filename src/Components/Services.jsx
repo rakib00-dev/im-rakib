a7670caa2a5b73764749ec10b4d9ed446398081b
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-02-06 05:28:28 +0600

    updated the services.jsx section content

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,5 +1,5 @@
 import Card from './Card';
-import './services.css';
+// import './services.css';
 
 const Services = () => {
   return (
@@ -10,25 +10,22 @@ const Services = () => {
           <div className="p-1 w-full flex justify-center items-center gap-12 flex-col m-auto md:p-6 lg:flex-row">
             {/* card1 */}
             <Card title="Basic">
-              <li>{'>'} Cutting and trimming clips</li>
-              <li>{'>'} Arranging video footage in a timeline</li>
-              <li>{'>'} Adding transitions between clips</li>
-              <li>{'>'} Basic color correction</li>
-              <li>{'>'} Audio enhancements</li>
+              <li>{'>'} Custom designs tailored to your brand</li>
+              <li>{'>'} Mobile-friendly, responsive layouts</li>
+              <li>{'>'} Built with modern web technologies</li>
+              <li>{'>'} Easy-to-manage CMS integration</li>
+              <li>{'>'} SEO optimized for better visibility</li>
             </Card>
 
             {/* card2 */}
             <Card title="Standard" price={70}>
               <li>
-                {'>'} <span className="text-[#e6b243] font-bold">Basic</span>{' '}
+                {'>'} <span className="text-[#e6b243] font-bold">Basic</span>
               </li>
-              <li>{'>'} Cutting and trimming clips</li>
-              <li>{'>'} Arranging video footage in a timeline</li>
-              <li>{'>'} Adding transitions between clips</li>
-              <li>
-                {'>'} Basic color correction{'  '}
-                <span className="text-[#e6b243]">...& More</span>{' '}
-              </li>
+              <li>{'>'} Setup and customization of platforms</li>
+              <li>{'>'} Integration of secure payment gateways</li>
+              <li>{'>'} Product catalog management</li>
+              <li>{'>'} Smooth animation and transition</li>
             </Card>
 
             {/* card3 */}
@@ -39,13 +36,10 @@ const Services = () => {
                   Basic + Standerd
                 </span>{' '}
               </li>
-              <li>{'>'} Special effects and animations</li>
-              <li>{'>'} Advanced color grading</li>
-              <li>{'>'} Custom transitions and effects</li>
-              <li>
-                {'>'} (VFX){' '}
-                <span className="text-[#e6b243]">...& Much More</span>{' '}
-              </li>
+              <li>{'>'} Bug fixing, performance optimization</li>
+              <li>{'>'} 24/7 support for any technical issues</li>
+              <li>{'>'} Features for better user interaction</li>
+              <li>{'>'} Frontent + Backend</li>
             </Card>
           </div>
         </div>
