701119a7f8c1245647ba72b322da720e4a286f92
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-02-06 08:28:16 +0600

    updated project section shadow blur, header section shine style changed, mobile responsive fix

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -158,7 +158,7 @@ const Header = () => {
       <header
         id="header-section"
         ref={header_section}
-        className="relative mt-[50px] pt-0 w-full"
+        className="relative mt-[50px] pt-0 w-full overflow-hidden"
       >
         <div className="p-4 w-full py-6 flex justify-center flex-wrap flex-col m-auto gap-20 items-center lg:gap-40 md:flex-row md:w-4/5">
           <div
